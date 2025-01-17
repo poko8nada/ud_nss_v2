fb1a45ffcd09b2d78d1504648a75054b73eaae08
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-01-17 11:57:19 +0900

    Update biome configuration and remove unused SVG files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import { Button, buttonVariants } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className=''>
       <main>
-        <section className='pt-6 flex justify-center'>
-          <div className='container text-center flex flex-col items-center gap-3'>
-            <Link href='/' className='bg-gray-200 py-3 px-6 rounded-lg'>
+        <section className='my-8 flex justify-center pt-6 md:my-12'>
+          <div className='container flex flex-col items-center gap-6 text-center'>
+            <Link
+              href='/'
+              className={cn(
+                buttonVariants({ size: 'lg', variant: 'secondary' }),
+                'text-md',
+              )}
+            >
               follow X
             </Link>
-            <h1 className='text-xl font-bold'>next-shadcn-supabase</h1>
-            <p className='text-sm text-gray-500'>
+            <h1 className='font-bold text-3xl'>next-shadcn-supabase</h1>
+            <p className='text-gray-500 text-sm'>
               next-shadcn-supabase burabura burabura burabura
             </p>
             <div className='space-x-4'>
@@ -35,6 +42,33 @@ export default function Home() {
               >
                 github
               </Link>
+            </div>
+          </div>
+        </section>
+        <section className='flex justify-center bg-slate-50 py-8 pt-6 md:py-12'>
+          <div className='container space-y-6'>
+            <div className='space-y-6'>
+              <h2 className='text-center font-bold text-3xl'>feature</h2>
+              <p className='mx-auto max-w-96 text-center text-muted-foreground'>
+                this is super feature buraburabura buraburabura buraburabura
+                buraburabura buraburabura
+              </p>
+            </div>
+            <div className='grid grid-cols-2 gap-4 space-y-6 md:grid-cols-3'>
+              <div className='flex flex-col items-center gap-1 rounded-md border bg-background p-4'>
+                <Image
+                  src='/images/next.svg'
+                  alt='Next.js Logo'
+                  width={40}
+                  height={40}
+                />
+                <div>
+                  <h3 className='text-center'>Next.js</h3>
+                  <p className='text-center text-muted-foreground text-sm'>
+                    App routerを使ったプロジェクト
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
